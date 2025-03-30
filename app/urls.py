d1a0f9c7a424b18fdc89d5ec9388c58f54f22c0b
letmeknow <158057929+enteryourworld@users.noreply.github.com>
2025-03-30 03:10:07 +0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.search_vk_companies, name='chat-view'),
-    # Добавьте другие маршруты по аналогии
+    path('', views.group_info, name='vk_group_search'),
 ]
